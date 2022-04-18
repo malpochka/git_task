9d9b1ad5da3b4933515b54e8cfb16185dd716140
Anna Ipatova <erosha1988@mail.ru>
2022-04-18 16:03:09 +0300

    initial commit

---
+++ src/test/java/by/itsuppotme/git_task/GitTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package by.itsuppotme.git_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
