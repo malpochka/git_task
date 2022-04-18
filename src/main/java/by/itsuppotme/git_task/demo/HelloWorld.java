0ef3e10abac8e8cc4a5e9ffc7e24980d7a041ba4
Anna Ipatova <erosha1988@mail.ru>
2022-04-18 16:12:01 +0300

    the second-branch

---
+++ src/main/java/by/itsuppotme/git_task/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package by.itsuppotme.git_task.demo;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+    @EventListener(ApplicationReadyEvent.class)
+    public void hi() {
+        System.out.println("Hi");
+    }
+}
